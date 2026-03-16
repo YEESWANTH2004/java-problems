a95ffed6853b956f8443dff9b0847572664cbfe6
YEESWANTH P L <144304627+YEESWANTH2004@users.noreply.github.com>
2026-03-16 16:07:56 +0530

    biweekly
    Added problem statement and examples for Kth Largest Element.

---
+++ KthLargestElement.java
@@ -0,0 +1,52 @@
+Given an integer array nums and an integer k, return the kth largest element in the array. 
+Note that it is the kth largest element in the sorted order, not the kth distinct element. Can you solve it without sorting? 
+
+Example 1: 
+Input: 
+nums = [3,2,1,5,6,4], k = 2 
+Output: 5 
+
+Example 2: 
+Input: 
+nums = [3,2,3,1,2,4,5,5,6], k = 4 
+Output: 4 
+
+Constraints: 
+ 1 <= k <= nums.length <= 10^5 
+ -10^4 <= nums[i] <= 10^4
+
+------------------------------------------------------------------------------------------------------------------------------
+import java.util.*;
+
+class Solution {
+
+    int solve(ArrayList<Integer> nums, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.offer(num);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(3, 2, 1, 5, 6, 4));
+        int k = 2;
+
+        Solution obj = new Solution();
+
+        int result = obj.solve(nums, k);
+
+        System.out.println("Kth largest element: " + result);
+    }
+}
